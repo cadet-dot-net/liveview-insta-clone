349580d4d67ed389d304c82d0f94a2fa626e48fd
cadet-dot-net <163072113+cadet-dot-net@users.noreply.github.com>
2024-10-27 22:24:29 +0000

    broadcast post updates - append new posts to the top of the feed

--- lib/finsta_web/live/home_live.ex
+++ lib/finsta_web/live/home_live.ex
@@ -36,6 +36,8 @@ defmodule FinstaWeb.HomeLive do
   @impl true
   def mount(_params, _session, socket) do
     if connected?(socket) do
+      Phoenix.PubSub.subscribe(Finsta.PubSub, "posts")
+      
       form =
         %Post{}
         |> Post.changeset(%{})
@@ -69,17 +71,29 @@ defmodule FinstaWeb.HomeLive do
     })
     |> Posts.save()
     |> case do
-      {:ok, _post} -> 
+      {:ok, post} -> 
         socket = 
           socket
           |> put_flash(:info, "Post created successfully!")
           |> push_navigate(to: ~p"/home")
           
+        Phoenix.PubSub.broadcast(Finsta.PubSub, "posts", {:new, Map.put(post, :user, user)})
+        
         {:noreply, socket}
         
       {:error, _changeset} ->
         {:noreply, socket}
     end
+  end
+  
+  @impl true
+  def handle_info({:new, post}, socket) do
+    socket =
+      socket
+      |> put_flash(:info, "#{post.user.email} just posted!")
+      |> stream_insert(:posts, post, at: 0)
+      
+    {:noreply, socket}
   end
   
   defp consume_files(socket) do
