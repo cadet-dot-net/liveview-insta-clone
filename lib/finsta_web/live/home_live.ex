18a64d3fb4a380b1711ec2c90149640ed8aae46e
cadet-dot-net <163072113+cadet-dot-net@users.noreply.github.com>
2024-10-27 19:12:35 +0000

    add Posts context - add form to submit a post - migrate posts table

--- lib/finsta_web/live/home_live.ex
+++ lib/finsta_web/live/home_live.ex
@@ -1,15 +1,26 @@
 defmodule FinstaWeb.HomeLive do
   use FinstaWeb, :live_view
+  alias Finsta.Posts.Post
   
   @impl true
   def render(assigns) do
     ~H"""
     <h1 class="test-2x1">Finsta</h1>
+    <.simple_form for={@form}>
+      <.input field={@form[:caption]} type="textarea" label="Caption" required />
+    </.simple_form>
     """
   end
   
   @impl true
   def mount(_params, _session, socket) do
+    form =
+      %Post{}
+      |> Post.changeset(%{})
+      |> to_form(as: "post")
+      
+    socket = assign(socket, form: form)
+    
     {:ok, socket}
   end
 end
